550142201e50eb5b252d33ece2e070ed1110749d
ifaamrillah <amrillah.work@gmail.com>
2024-12-29 13:09:57 +0700

    feat: add task entity

--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,1 +1,14 @@
-export class Task {}
+export const TASK_STATUS = ['TODO', 'ON_PROGRESS', 'DONE', 'ARCHIVED'] as const;
+
+export type TypeTaskStatus = (typeof TASK_STATUS)[number];
+
+export class Task {
+  id: number;
+  title: string;
+  description: string;
+  status: TypeTaskStatus;
+  ownerId: number;
+  createdAt: number; // unix timestamp
+  updatedAt: number; // unix timestamp
+  isDeleted: boolean;
+}
