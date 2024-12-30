06fe208fc789695ec92f8f519a635fdda6b42fbf
ifaamrillah <amrillah.work@gmail.com>
2024-12-30 10:15:01 +0700

    feat: unit test create task service

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { Task } from './entities/task.entity';
 
 describe('TasksService', () => {
   let service: TasksService;
@@ -14,5 +16,22 @@ describe('TasksService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('create: should create task', () => {
+    const createTaskDto: CreateTaskDto = {
+      title: 'Task 1',
+      description: 'Description 1',
+    };
+
+    const task: Task = service.create(createTaskDto);
+
+    expect(task.title).toBe(createTaskDto.title);
+    expect(task.description).toBe(createTaskDto.description);
+    expect(task.isDeleted).toBe(false);
+
+    expect(task.id).toBe(2);
+
+    expect(service.findAll()).toHaveLength(2);
   });
 });
